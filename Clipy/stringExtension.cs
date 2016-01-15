a69df37b18e8809802f075d640f2d7f80ad999bc
Venj Chu <ersaclarke@gmail.com>
2016-01-15 15:11:39 +0800

    add missing file.

---
+++ Clipy/stringExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Clipy
+{
+    public static class StringExtension
+    {
+        public static string FirstLine(this string self)
+        {
+            var lines = self.Split(new Char[]{'\n'});
+            return lines[0];
+        }
+    }
+}
