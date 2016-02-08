53f873f8d15e26bc5af16f7fb5a83a7e07810a5e
Venj Chu <ersaclarke@gmail.com>
2016-02-08 10:16:26 +0800

    Added basic implementation of inputbox.

---
+++ Clipy/InputBox.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Clipy
+{
+    partial class InputBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InputBox));
+            this.promptLabel = new System.Windows.Forms.Label();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.okButton = new System.Windows.Forms.Button();
+            this.contentBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // promptLabel
+            // 
+            resources.ApplyResources(this.promptLabel, "promptLabel");
+            this.promptLabel.Name = "promptLabel";
+            // 
+            // cancelButton
+            // 
+            resources.ApplyResources(this.cancelButton, "cancelButton");
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // okButton
+            // 
+            resources.ApplyResources(this.okButton, "okButton");
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.okButton.Name = "okButton";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // contentBox
+            // 
+            resources.ApplyResources(this.contentBox, "contentBox");
+            this.contentBox.Name = "contentBox";
+            // 
+            // InputBox
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.contentBox);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.promptLabel);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "InputBox";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.InputBox_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label promptLabel;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.TextBox contentBox;
+    }
+}
---
+++ Clipy/InputBox.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clipy
+{
+    public partial class InputBox : Form
+    {
+        private string Prompt { get; set; }
+        private string Title { get; set; }
+        private string DefaultResponse { get; set; }
+
+        public InputBox(string prompt, string title, string defaultResponse) : this()
+        {
+            Prompt = prompt;
+            Title = title;
+            DefaultResponse = defaultResponse;
+            // UI
+            Text = title;
+            promptLabel.Text = Prompt;
+            contentBox.Text = defaultResponse;
+            contentBox.SelectAll();
+        }
+
+        public InputBox()
+        {
+            InitializeComponent();
+        }
+
+        private void InputBox_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                DialogResult = DialogResult.Cancel;
+            }
+        }
+    }
+}
